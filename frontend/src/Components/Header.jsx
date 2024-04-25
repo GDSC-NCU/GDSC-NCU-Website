abdfa3d4a13187c6d9c0680b5e6988ebdda49204
Godcoder696 <117354227+Godcoder696@users.noreply.github.com>
2024-04-25 23:13:42 +0530

    Link added

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -2,19 +2,20 @@ import React from 'react';
 import '../App.css';
 import LOGO from '../Assets/logo.png';
 import './Header.css';
+import {Link} from 'react-router-dom';
 
 
 function Header() {
   return (
     <div>
       <header className="page-header">
-        <a href="/">Home</a>
-        <a href="/About">About</a>
-        <a href="/Event">Event</a>
-        <a href="/App"><img alt="logo" src={LOGO} className="logo-img" /></a>
-        <a href="/Team">Team</a>
-        <a href="/RoadMap">Road Map</a>
-        <a href="/Blog">Blog</a>
+        <Link to="/">Home</Link>
+        <Link to="/About">About</Link>
+        <Link to="/Event">Event</Link>
+        <Link to="/App"><img alt="logo" src={LOGO} className="logo-img" /></Link>
+        <Link to="/Team">Team</Link>
+        <Link to="/RoadMap">Road Map</Link>
+        <Link to="/Blog">Blog</Link>
       </header>
     </div>
   );
